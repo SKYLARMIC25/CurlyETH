a8ff8756a78464afebe5d4b93ba0114950df6098
SKYLARMIC25 <78185477+SKYLARMIC25@users.noreply.github.com>
2021-09-29 10:15:25 +0800

    Initialize Improve-connecting

---
+++ src/connectProvider/web3Provider/web3-provider/events.js
@@ -0,0 +1,7 @@
+export default {
+  SHOW_WEB3_CONFIRM_MODAL: 'showWeb3Wallet',
+  SHOW_TX_CONFIRM_MODAL: 'showTxConfirmModal',
+  SHOW_MSG_CONFIRM_MODAL: 'showMessageConfirmModal',
+  SHOW_TX_SIGN_MODAL: 'showTxSignModal',
+  WALLET_NOT_CONNECTED: 'walletNotConnected'
+};
